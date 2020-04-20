db3a1d4161ab5f32ba1b7e6e2a8a68a03e1becd0
MaudG <maudg35@outlook.fr>
2020-04-20 11:12:16 +0200

    add Dao Reunion 2

--- src/main/java/dao/DaoReunion.java
+++ src/main/java/dao/DaoReunion.java
@@ -1,4 +1,22 @@
 package dao;
 
+import jpa.EntityManagerHelper;
+import jpa.Reunion;
+
+import javax.persistence.EntityManager;
+import java.util.List;
+
 public class DaoReunion {
+    private EntityManager manager = EntityManagerHelper.getEntityManager();
+
+    public List<Reunion> getAll() {
+        return manager.createQuery("Select r From Reunion r", Reunion.class).getResultList();
+    }
+
+    public Reunion createReunion(Reunion reunion) {
+        EntityManagerHelper.beginTransaction();
+        EntityManagerHelper.getEntityManager().persist(reunion);
+        EntityManagerHelper.commit();
+        return reunion;
+    }
 }
